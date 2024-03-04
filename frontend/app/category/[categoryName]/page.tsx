4db07a2d66daf3becfb0d991c3a401628193d2d0
Cameron Lee <cal26@sfu.ca>
2024-03-03 20:19:07 -0800

    added pagination bar on top and bottom of page, synchronized their page numbers

--- frontend/app/category/[categoryName]/page.tsx
+++ frontend/app/category/[categoryName]/page.tsx
@@ -5,9 +5,17 @@ import { getPageEnumVal } from "@/app/pages";
 import ItemGrid from "@/components/ItemGrid/ItemGrid";
 import TopNavbar from "@/components/TopNavbar/TopNavbar";
 import { Pagination } from "@nextui-org/react";
+import { useState } from "react";
 
 function page({ params }: { params: { categoryName: string } }) {
 	const categoryNameEnumVal = getPageEnumVal(params.categoryName);
+
+	const [curPaginationPage, setCurPaginationPage] = useState(1);
+	const totalPaginationPages = 10;
+
+	const onPaginationChange = (page: number) => {
+		setCurPaginationPage(page);
+	};
 
 	if (categoryNameEnumVal) {
 		return (
@@ -16,6 +24,16 @@ function page({ params }: { params: { categoryName: string } }) {
 				<main className="flex flex-col items-center mb-16">
 					<div className="flex justify-center items-center text-center w-full h-60 border border-blue-500">
 						Representative image
+					</div>
+					<div className="flex justify-center items-center text-center my-4">
+						<Pagination
+							showControls
+							total={totalPaginationPages}
+							page={curPaginationPage}
+							onChange={(page: number) => {
+								onPaginationChange(page);
+							}}
+						/>
 					</div>
 					<div className="flex flex-1 w-full mb-10">
 						<div
@@ -30,7 +48,15 @@ function page({ params }: { params: { categoryName: string } }) {
 						/>
 					</div>
 					<div className="flex justify-center items-center text-center">
-						<Pagination showControls total={10} initialPage={1} />
+						<Pagination
+							showControls
+							total={totalPaginationPages}
+							page={curPaginationPage}
+							onChange={(page: number) => {
+								onPaginationChange(page);
+								window.scrollTo({ top: 0, behavior: "smooth" });
+							}}
+						/>
 					</div>
 				</main>
 			</>
