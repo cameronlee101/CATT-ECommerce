4ab17fc446c901349ac81cb8df8871c09169ec92
Cameron Lee <cal26@sfu.ca>
2024-03-11 16:17:34 -0700

    created product listing creation page

--- frontend/app/product-listings/page.tsx
+++ frontend/app/product-listings/page.tsx
@@ -1,4 +1,5 @@
 import TopNavbar from "@/components/TopNavbar/TopNavbar";
+import Link from "next/link";
 import React from "react";
 
 function page() {
@@ -7,8 +8,14 @@ function page() {
 			<TopNavbar />
 			<main className="flex flex-col items-center mb-16">
 				<h3 className="mt-8 mx-4 text-2xl">My Product Listings</h3>
-				<div className="flex flex-1 justify-center w-full mb-10 mt-8">
+				<div className="flex flex-col flex-1 text-center w-full mb-10 mt-8">
 					<p>TODO: create components to fetch product listing for this user</p>
+					<Link
+						href={"/product-listings/create-listing"}
+						className="text-blue-500"
+					>
+						CREATE NEW LISTING
+					</Link>
 				</div>
 			</main>
 		</>
