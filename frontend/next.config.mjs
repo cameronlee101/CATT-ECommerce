154f158cad0e4d92ac4e6d19b2093465a26a294f
Cameron Lee <cal26@sfu.ca>
2024-03-12 22:03:22 -0700

    began implementing paypal sdk

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		SECRET_KEY: process.env.SECRET_KEY,
 		BACKEND_SERVER_BASE_URL: process.env.BACKEND_SERVER_BASE_URL,
+		PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+		PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET,
 	},
 };
 
