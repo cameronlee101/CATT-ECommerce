700d594578ca4836146b32c11a0d1a1b60035495
Cameron Lee <cal26@sfu.ca>
2024-03-14 13:46:01 -0700

    refactored google login button into a component

---
+++ frontend/components/GoogleLoginButton/GoogleLoginButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { login } from "@/app/auth";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useRouter } from "next/navigation";
+
+function GoogleLoginButton() {
+	const router = useRouter();
+
+	return (
+		<GoogleLogin
+			onSuccess={(credentialResponse: CredentialResponse) =>
+				login(credentialResponse).then(() => router.push("/"))
+			}
+			onError={() => {
+				console.log("Login Failed");
+			}}
+		/>
+	);
+}
+
+export default GoogleLoginButton;
