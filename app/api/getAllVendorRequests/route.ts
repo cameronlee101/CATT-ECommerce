4d230678cc21a960979995a08973e91f92056dbd
Cameron Lee <cal26@sfu.ca>
2024-09-28 17:00:33 -0700

    fix caching issue in vendor requests

--- app/api/getAllVendorRequests/route.ts
+++ app/api/getAllVendorRequests/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 const db = require("@/app/api/db");
 
+// theres some weird stuff with nextjs caching the result of this api call which causes a headache, this stops caching
+export const revalidate = 0;
+
 export async function GET(req: NextRequest) {
   try {
     // Fetch all vendor requests using the helper function
